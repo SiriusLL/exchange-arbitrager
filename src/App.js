a3e8505150cd7fe390f46153cd78d8a818b508a1
SiriusLL <sirius.love.light@gmail.com>
2021-08-15 15:31:36 -0600

    save

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+//1. Import coingecko-api
+const CoinGecko = require("coingecko-api");
+
+//2. Initiate the CoinGecko API Client
+const CoinGeckoClient = new CoinGecko();
+
+//3. Make calls
+const gecko = async () => {
+  let data = await CoinGeckoClient.ping();
+  console.log("data", data);
+  return data;
+};
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">{gecko}</div>;
 }
 
 export default App;
