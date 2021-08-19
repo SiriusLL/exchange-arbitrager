35a4864d751ce88cd9011525d4b7767d96c238a3
SiriusLL <sirius.love.light@gmail.com>
2021-08-18 23:24:18 -0600

    refactor

--- src/App.js
+++ src/App.js
@@ -2,30 +2,12 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Form from "./components/Form.jsx";
 
-//1. Import coingecko-api
-const CoinGecko = require("coingecko-api");
-
-//2. Initiate the CoinGecko API Client
-const CoinGeckoClient = new CoinGecko();
-
-//3. Make calls
-
 function App() {
-  const [ping, setPing] = useState({});
-  useEffect(() => {
-    const gecko = async () => {
-      let data = await CoinGeckoClient.coins.all();
-      // console.log("data", data);
-      // data = JSON.stringify(data);
-      data = data;
-      console.log("data", data);
-      setPing(data);
-      // return data;
-    };
-    gecko();
-  }, []);
-  console.log("ping", typeof ping.data);
-  return <div className="App">{ping.data.map((index) => index.id)}</div>;
+  return (
+    <>
+      <Form />
+    </>
+  );
 }
 
 export default App;
--- src/components/PriceApi.jsx
+++ src/components/PriceApi.jsx
@@ -1,5 +1,29 @@
+import React, { useEffect, useState } from "react";
+
+//1. Import coingecko-api
+const CoinGecko = require("coingecko-api");
+
+//2. Initiate the CoinGecko API Client
+const CoinGeckoClient = new CoinGecko();
+
+//3. Make calls
+
 const PriceApi = () => {
-  return <>exchange price</>;
+  const [ping, setPing] = useState({});
+  useEffect(() => {
+    const gecko = async () => {
+      let data = await CoinGeckoClient.coins.all();
+      // console.log("data", data);
+      // data = JSON.stringify(data);
+      data = data;
+      console.log("data", data);
+      setPing(data);
+      // return data;
+    };
+    gecko();
+  }, []);
+  console.log("ping", typeof ping.data);
+  return <div className="App">{ping.data.map((index) => index.id)}</div>;
 };
 
 export default PriceApi;
