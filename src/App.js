ff7b72d6a0dfab1ae62b3094ad71b3cca0d962b2
SiriusLL <sirius.love.light@gmail.com>
2021-08-18 23:15:24 -0600

    price api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import Form from "./components/Form.jsx";
 
 //1. Import coingecko-api
 const CoinGecko = require("coingecko-api");
---
+++ src/components/PriceApi.jsx
@@ -0,0 +1,5 @@
+const PriceApi = () => {
+  return <>exchange price</>;
+};
+
+export default PriceApi;
